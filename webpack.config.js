931cd4bc8b5ab6182fd928094c20f0e403bee3a0
Oxide <mohammedhaider15mo@gmail.com>
2024-04-01 22:00:36 +0300

    add webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,22 @@ const path = require("path");
 HtmlWebpackPlugin
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
     },
+    devServer: {
+        watchFiles: ["./src/index.html"],
+        static: "./dist"
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "/src/index.html",
+            inject: "body"
+        })
+    ]
 }
